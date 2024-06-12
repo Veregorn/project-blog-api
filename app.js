da6cf68944b25aa646cb177c68883f9915f58e27
Veregorn <rjbarco@gmail.com>
2024-06-12 07:04:29 +0200

    Routes created

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose'); // Import mongoose
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const router = require('./routes/index');
 
 const app = express();
 
@@ -33,8 +32,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api', router);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,40 @@
 const express = require('express');
 const router = express.Router();
 
+// Import the controllers
+const postController = require('../controllers/postController');
+const commentController = require('../controllers/commentController');
+const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.send('Welcome to the Express API');
 });
+
+// Auth routes
+router.post('/auth/login', authController.login);
+router.post('/auth/register', authController.register);
+
+// Post routes
+router.get('/posts', postController.getAllPosts);
+router.get('/posts/:id', postController.getPost);
+router.post('/posts', postController.createPost);
+router.put('/posts/:id', postController.updatePost);
+router.delete('/posts/:id', postController.deletePost);
+
+// Comment routes
+router.get('/comments', commentController.getAllComments);
+router.get('/comments/:id', commentController.getComment);
+router.post('/comments', commentController.createComment);
+router.put('/comments/:id', commentController.updateComment);
+router.delete('/comments/:id', commentController.deleteComment);
+
+// User routes
+router.get('/users', userController.getAllUsers);
+router.get('/users/:id', userController.getUser);
+router.post('/users', userController.createUser);
+router.put('/users/:id', userController.updateUser);
+router.delete('/users/:id', userController.deleteUser);
 
 module.exports = router;
